2d85b2f487d3c84f5801391a28e843354d968086
charuraj88 <85609806+charuraj88@users.noreply.github.com>
2023-11-21 13:11:36 +0530

    Delete Downloads/crypto-exchange-master directory

--- Downloads/crypto-exchange-master/update-version.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-const fs = require('fs')
-const os = require('os')
-const prependFile = require('prepend-file')
-
-let { version } = JSON.parse(fs.readFileSync('./package.json', 'utf8'))
-
-prependFile.sync('./CHANGELOG.md', `# v${version}${os.EOL}`)
